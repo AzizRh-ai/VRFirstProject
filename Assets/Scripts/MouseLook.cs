2f204ffd1e746e53e3b87c9040c1e4605b840fde
AzizRh-ai <121114268+AzizRh-ai@users.noreply.github.com>
2023-01-19 15:30:30 +0100

    Added Script FakeMouse

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -10,4 +10,24 @@ public class MouseLook : MonoBehaviour
 
     float pitch;
     float yaw;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        horizontalLook.action.performed += HandleHorizontalLookChange;
+        verticalLook.action.performed += HandleVerticalLookChange;
+    }
+
+    private void HandleHorizontalLookChange(InputAction.CallbackContext obj)
+    {
+        yaw += obj.ReadValue<float>();
+        transform.localRotation = Quaternion.AngleAxis(yaw * lookSpeed, Vector3.up);
+    }
+
+    private void HandleVerticalLookChange(InputAction.CallbackContext obj)
+    {
+        pitch -= obj.ReadValue<float>();
+        headCamTransform.localRotation = Quaternion.AngleAxis(pitch * lookSpeed, Vector3.right);
+
+    }
 }
